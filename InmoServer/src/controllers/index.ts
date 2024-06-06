16f243b4405fd607dd33be87258da918e144a7b5
carmesotuyo <carmela.sotuyo@gmail.com>
2024-06-06 14:16:34 -0300

    agrego interfaz de queue service

--- InmoServer/src/controllers/index.ts
+++ InmoServer/src/controllers/index.ts
@@ -1,5 +1,6 @@
 // src/controllers/index.ts
 import { PropertyController } from './propertyController';
 import { propertyService } from '../services/propertyService';
+import { queueService } from '../services/queueService';
 
-export const propertyController = new PropertyController(propertyService);
+export const propertyController = new PropertyController(propertyService, queueService);
--- InmoServer/src/controllers/propertyController.ts
+++ InmoServer/src/controllers/propertyController.ts
@@ -1,14 +1,14 @@
 // src/controllers/propertyController.ts
 import { Request, Response } from 'express';
 import { PropertyService } from '../servicesInterfaces/propertyService';
-import * as queueService from '../services/queueService';
+import { QueueService } from '../servicesInterfaces/queueService';
 import { getErrorMessage } from '../utils/handleError';
 // import { PropertyDTO } from '../dtos/propertyDTO';
 // import { validateOrReject } from 'class-validator';
 // import { plainToClass } from 'class-transformer';
 
 export class PropertyController {
-    constructor(private propertyService: PropertyService) {}
+    constructor(private propertyService: PropertyService, private queueService: QueueService) {}
   
     public createProperty = async (req: Request, res: Response) => {
       try {
@@ -16,7 +16,7 @@ export class PropertyController {
         // await validateOrReject(propertyDto);
   
         const property = await this.propertyService.createProperty(req.body); //propertyDto
-        queueService.addJobToQueue(property.toJSON());
+        this.queueService.addJobToQueue(property.toJSON());
         res.status(201).json(property);
       } catch (error: any) {
         res.status(400).json({ message: 'Error creating property', error: getErrorMessage(error) });
