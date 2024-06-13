74a1a5fd5223b0d8319f40584f5db69f19b54b3b
Fernando Spillere <f.spillere269@gmail.com>
2024-06-13 01:53:01 -0300

    se agrega middleware

--- InmoServer/src/routes/auhtRoutes.ts
+++ InmoServer/src/routes/auhtRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { auth } from 'express-openid-connect';
 import { authController } from '../controllers';
 import { authConfig } from '../config/authConfig';
+import { authenticateJWT } from '../middleware/authMiddleware';
 
 const router = Router();
 const { login, callback } = authController;
