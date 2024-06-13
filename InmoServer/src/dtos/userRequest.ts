fc95c988c5637bbb725b073c1bbbe7f14889f2d7
Fernando Spillere <f.spillere269@gmail.com>
2024-06-13 01:34:23 -0300

    se crea userDto

---
+++ InmoServer/src/dtos/userRequest.ts
@@ -0,0 +1,10 @@
+export interface UserRequest {
+    document: string;
+    document_type: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone_number: string;
+    role: string;
+    auth0_id: string;
+  }
