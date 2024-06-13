f673b68d19ef675bdd18af9e19b77ae4550c5950
Fernando Spillere <f.spillere269@gmail.com>
2024-06-13 01:34:06 -0300

    Se arregla authcontroller

--- InmoServer/src/controllers/authController.ts
+++ InmoServer/src/controllers/authController.ts
@@ -2,13 +2,10 @@ import { Request, Response } from 'express';
 import { UserService } from '../services/authService';
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
+//import { User } from '../data-access/user';
 
 export class AuthController {
-  private service: UserService;
-
-  constructor(service?: UserService) {
-    this.service = service ? service : new UserService();
-  }
+  constructor(private service: UserService) {}
 
   public login = async (req: Request, res: Response) => {
     const { username, password } = req.body;
@@ -26,6 +23,8 @@ export class AuthController {
 
       const { access_token } = tokenResponse.data;
 
+      console.log('Access token:', access_token);
+
       // Verificar token con Auth0
       const userInfoResponse = await axios.get(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
         headers: {
@@ -35,8 +34,41 @@ export class AuthController {
 
       const { email } = userInfoResponse.data;
 
+      //console.log('User info:', userInfoResponse.data);
+
+      // const newUser = {
+      //   email: 'spillere.fer',
+      //   role: 'admin',
+      //   last_name: 'Spillere',
+      //   first_name: 'Fernando',
+      // };
+
+      //const users = await this.service.getAllUsers();
+      // console.log('Users:');
+      // if (users) {
+      //   console.log(users);
+      // } else {
+      //   console.log('No users found');
+      // }
+      // const user = {
+      //   document: '5123123-3',
+      //   document_type: 'CI',
+      //   first_name: 'Fernando',
+      //   last_name: 'Spillere',
+      //   email: 'spillere.fer@gmail.com',
+      //   phone_number: '098123123',
+      //   role: 'Administrador',
+      //   auth0_id: 'auth0|66613ea1638a534c6a3d71fd',
+      // };
+
+      // const usuarioCreado = await this.service.createUser(user);
+      // console.log('User creado ======= ');
+      // console.log(usuarioCreado);
+
       // Obtener usuario de la base de datos
-      const user = await this.service.getUserByEmail(email);
+      const user: any = await this.service.getUserByEmail(email);
+
+      //const user = await User.findOne({ where: { email } });
 
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
@@ -44,7 +76,7 @@ export class AuthController {
 
       // Crear nuestro propio JWT
       const tokenPayload = {
-        id: user.id,
+        id: user.auth0_id,
         email: user.email,
         role: user.role,
       };
