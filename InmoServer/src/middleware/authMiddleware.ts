7960b17cbd8bc391d66f14b380a33e5a5c641976
Fernando Spillere <f.spillere269@gmail.com>
2024-06-12 23:04:01 -0300

    fix de jwt

--- InmoServer/src/middleware/authMiddleware.ts
+++ InmoServer/src/middleware/authMiddleware.ts
@@ -1,25 +1,30 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-export const checkRole = (requiredRole: string) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split(' ')[1];
+export const authenticateJWT =
+  (allowedRoles?: string[]) => (req: any, res: any, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
-      return res.status(401).json({ error: 'No token provided' });
-    }
 
-    try {
-      const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
 
-      if (decoded.role !== requiredRole) {
-        return res.status(403).json({ error: 'Access denied' });
-      }
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
 
-      req.user = decoded;
-      next();
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
+      jwt.verify(token, process.env.JWT_SECRET! , (err: any, user: any) => {
+        if (err) {
+          return res.status(403).json({ error: 'Access denied' });
+        }
+
+        if (allowedRoles && (!user.role || !allowedRoles.includes(user.role))) {
+          return res.status(403).json({
+            message: "Insufficient permissions.",
+          });
+        }
+
+        req.user = user;
+        next();
+      });
+    } else {
+      res.status(401).json({ error: 'No token provided' });
     }
   };
-};
