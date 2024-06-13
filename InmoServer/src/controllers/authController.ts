2011fc50e1c440d5073eaca3894d64823f431c71
Fernando Spillere <f.spillere269@gmail.com>
2024-06-12 22:49:37 -0300

    Se corrije inyeccion de dependencias

--- InmoServer/src/controllers/authController.ts
+++ InmoServer/src/controllers/authController.ts
@@ -1,86 +1,66 @@
 import { Request, Response } from 'express';
+import { UserService } from '../services/authService';
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
 
-export const login = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+export class AuthController {
+  private service: UserService;
 
-  try {
-    // Autenticación con Auth0
-    const tokenResponse = await axios.post(
-      `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
-      {
-        grant_type: "password",
+  constructor(service?: UserService) {
+    this.service = service ? service : new UserService();
+  }
+
+  public login = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    try {
+      // Autenticación con Auth0
+      const tokenResponse = await axios.post(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
+        grant_type: 'password',
         username,
         password,
         audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
         client_id: process.env.AUTH0_CLIENT_ID,
         client_secret: process.env.AUTH0_CLIENT_SECRET,
-        scope: "openid profile email",
-      }
-    );
+        scope: 'openid profile email',
+      });
 
-    const { access_token } = tokenResponse.data;
+      const { access_token } = tokenResponse.data;
 
-    // Verificar token con Auth0
-    const userInfoResponse = await axios.get(
-      `https://${process.env.AUTH0_DOMAIN}/userinfo`,
-      {
+      // Verificar token con Auth0
+      const userInfoResponse = await axios.get(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
         headers: {
           Authorization: `Bearer ${access_token}`,
         },
+      });
+
+      const { email } = userInfoResponse.data;
+
+      // Obtener usuario de la base de datos
+      const user = await this.service.getUserByEmail(email);
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
       }
-    );
 
-    const { email } = userInfoResponse.data;
+      // Crear nuestro propio JWT
+      const tokenPayload = {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      };
 
-    // Obtener usuario de la base de datos
-    //const user = await findUserByEmail(email);
-    const user = { id: 1, email, role: 'admin' };
+      const token = jwt.sign(tokenPayload, process.env.JWT_SECRET!, {
+        expiresIn: '1h',
+      });
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      res.json({ token });
+    } catch (error: any) {
+      console.error('Error during login request:', error.response ? error.response.data : error.message);
+      res.status(500).json({ error: error.response ? error.response.data : error.message });
     }
+  };
 
-    // Crear nuestro propio JWT
-    const tokenPayload = {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-    };
-
-    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET!, {
-      expiresIn: '1h',
-    });
-
-    res.json({ token });
-  } catch (error: any) {
-    console.error('Error during login request:', error.response ? error.response.data : error.message);
-    res.status(500).json({ error: error.response ? error.response.data : error.message });
-  }
-};
-
-// export const profile = async (req: Request, res: Response) => {
-//   const { authorization } = req.headers;
-
-//   if (!authorization) {
-//     return res.status(401).json({ error: 'Authorization header is missing' });
-//   }
-
-//   try {
-//     const userInfoResponse = await axios.get(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
-//       headers: {
-//         Authorization: authorization,
-//       },
-//     });
-
-//     res.json(userInfoResponse.data);
-//   } catch (error: any) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-export const callback = (req: Request, res: Response) => {
-  res.send('Login successful. You can close this tab.');
-};
+  public callback = (req: Request, res: Response) => {
+    res.send('Login successful. You can close this tab.');
+  };
+}
