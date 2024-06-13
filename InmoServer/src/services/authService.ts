f315d12161de151b00b429259d89fbe0e7754897
Fernando Spillere <f.spillere269@gmail.com>
2024-06-13 01:35:25 -0300

    Se arregla authService

--- InmoServer/src/services/authService.ts
+++ InmoServer/src/services/authService.ts
@@ -1,27 +1,45 @@
 import { User } from '../data-access/user';
 import { Op } from 'sequelize';
+import { UserRequest } from '../dtos/userRequest';
 
 export class UserService  {
-    async createUser(data: any): Promise<InstanceType<typeof User>> {
+    async createUser(data: UserRequest): Promise<InstanceType<typeof User>> {
       if (!data) throw Error('Data incorrecta, DTO vacio');
       const userObject = { ...data };
       return await User.create(userObject);
     }
-  
-    async getUserById(id: number): Promise<InstanceType<typeof User> | null> {
-      const user = await User.findByPk(id);
-      if (!user) {
-        throw new Error('User not found');
-      }
-      return user;
-    }
 
-    async getUserByEmail (email: string): Promise<InstanceType<typeof User> | null> {
-        const user = await User.findOne({ where: { email } });
+    async getUserByEmail (email: string):Promise<InstanceType<typeof User> | null> {
+        const user = await User.findByPk(email);
         if (!user) {
           throw new Error('User not found');
         }
         return user;
-      }
+    }
+
+    async getAllUsers(): Promise<InstanceType<typeof User>[]> {
+        const users = await User.findAll();
+        if (!users) {
+            throw new Error('User not found');
+          }
+        return users;
+    }
+
+    // async updateUser(email: string, data: UserRequest): Promise<InstanceType<typeof User>> {
+    //     const user = await User.findByPk(email);
+    //     if (!user) {
+    //       throw new Error('User not found');
+    //     }
+    //     return await user.update(data);
+    // }
+
+    // async deleteUser(email: string): Promise<void> {
+    //     const user = await User.findByPk(email);
+    //     if (!user) {
+    //       throw new Error('User not found');
+    //     }
+    //     await user.destroy();
+    // }
+    
     
   }
