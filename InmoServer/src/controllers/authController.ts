2a895da82d3d1a2a505fb89ccf8df42b4a4b758a
Fernando Spillere <f.spillere269@gmail.com>
2024-06-13 01:37:29 -0300

    Clean code en authcontroller

--- InmoServer/src/controllers/authController.ts
+++ InmoServer/src/controllers/authController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { UserService } from '../services/authService';
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
-//import { User } from '../data-access/user';
 
 export class AuthController {
   constructor(private service: UserService) {}
@@ -33,42 +32,7 @@ export class AuthController {
       });
 
       const { email } = userInfoResponse.data;
-
-      //console.log('User info:', userInfoResponse.data);
-
-      // const newUser = {
-      //   email: 'spillere.fer',
-      //   role: 'admin',
-      //   last_name: 'Spillere',
-      //   first_name: 'Fernando',
-      // };
-
-      //const users = await this.service.getAllUsers();
-      // console.log('Users:');
-      // if (users) {
-      //   console.log(users);
-      // } else {
-      //   console.log('No users found');
-      // }
-      // const user = {
-      //   document: '5123123-3',
-      //   document_type: 'CI',
-      //   first_name: 'Fernando',
-      //   last_name: 'Spillere',
-      //   email: 'spillere.fer@gmail.com',
-      //   phone_number: '098123123',
-      //   role: 'Administrador',
-      //   auth0_id: 'auth0|66613ea1638a534c6a3d71fd',
-      // };
-
-      // const usuarioCreado = await this.service.createUser(user);
-      // console.log('User creado ======= ');
-      // console.log(usuarioCreado);
-
-      // Obtener usuario de la base de datos
       const user: any = await this.service.getUserByEmail(email);
-
-      //const user = await User.findOne({ where: { email } });
 
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
