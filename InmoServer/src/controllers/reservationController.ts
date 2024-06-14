7608e09b6b2b87b866e04daa42c8e40662d5ae0d
Fernando Spillere <f.spillere269@gmail.com>
2024-06-14 03:30:46 -0300

    se agrega logs a reservation

--- InmoServer/src/controllers/reservationController.ts
+++ InmoServer/src/controllers/reservationController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express';
 import { ReservationService } from '../interfaces/services/reservationService';
 import { QueueService } from '../interfaces/services/queueService';
 import { getErrorMessage } from '../utils/handleError';
-
+import logger from '../config/logger';
+import { log } from 'console';
 export class ReservationController {
   constructor(
     private reservationService: ReservationService,
@@ -11,10 +12,12 @@ export class ReservationController {
 
   public createReservation = async (req: Request, res: Response) => {
     try {
-      const reservation = await this.reservationService.createReservation(req.body); //Reservation request
+      const reservation: any = await this.reservationService.createReservation(req.body); //Reservation request
       this.queueService.addJobToQueue(reservation.toJSON());
+      logger.info(`Reservation created - code: ${reservation.reservationCode}`);
       res.status(201).json(reservation);
     } catch (error: any) {
+      logger.error('Error creating reservation', { error: getErrorMessage(error) });
       res.status(400).json({
         message: 'Error creating reservation',
         error: getErrorMessage(error),
@@ -25,21 +28,21 @@ export class ReservationController {
   public getReservation = async (req: Request, res: Response) => {
     try {
       const { email, reservationCode } = req.query;
-
       if (!email || !reservationCode) {
         res.status(400).json({ message: 'Email y código de reserva son requeridos' });
         return;
       }
 
-      const reservation = await this.reservationService.getReservationByEmailAndCode(email as string, reservationCode as string);
+      const reservation: any = await this.reservationService.getReservationByEmailAndCode(email as string, reservationCode as string);
 
       if (!reservation) {
         res.status(404).json({ message: 'Reserva no encontrada' });
         return;
       }
-
+      logger.info(`Reservation fetched - code: ${reservation.reservationCode}`);
       res.status(200).json(reservation);
     } catch (error: any) {
+      logger.error('Error fetching reservation', { error: getErrorMessage(error) });
       res.status(500).json({
         message: 'Error consultando la reserva',
         error: getErrorMessage(error),
@@ -55,10 +58,11 @@ export class ReservationController {
         return;
       }
 
-      const reservation = await this.reservationService.cancelReservation(email, reservationCode);
-
+      const reservation: any = await this.reservationService.cancelReservation(email, reservationCode);
+      logger.info(`Reservation canceled - code: ${reservation.reservationCode}`);
       res.status(200).json(reservation);
     } catch (error: any) {
+      logger.error('Error canceling reservation', { error: getErrorMessage(error) });
       res.status(400).json({
         message: 'Error cancelando la reserva',
         error: getErrorMessage(error),
@@ -69,9 +73,10 @@ export class ReservationController {
     try {
       const filters = req.query;
       const reservations = await this.reservationService.getReservationsAdmin(filters);
-
+      logger.info(`Reservations fetched - filters: ${JSON.stringify(filters)}`);
       res.status(200).json(reservations);
     } catch (error: any) {
+      logger.error('Error fetching reservations', { error: getErrorMessage(error) });
       res.status(500).json({
         message: 'Error consultando las reservas',
         error: getErrorMessage(error),
