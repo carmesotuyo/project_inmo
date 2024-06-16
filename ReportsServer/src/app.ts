74aa55663d4693e49bcb46d077b4a667fd36dbdd
Fernando Spillere <f.spillere269@gmail.com>
2024-06-16 04:07:57 -0300

    se corrije error se conexion a mongo

--- ReportsServer/src/app.ts
+++ ReportsServer/src/app.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import * as matchController from "./controllers/matchController";
 import Queue from 'bull';
-import * as matchService from './services/matchService';
 
 console.log("Started Reports Server");
 
@@ -11,34 +9,50 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 // Conectarse a la misma cola que el productor
 // TODO cambiar todo matches por reports o lo que sea:
 const myQueue = new Queue('reports', 'redis://127.0.0.1:6378');
 
 app.use(express.json());
 
-// TODO poner los controllers especificos:
-app.get("/matches", matchController.getAllMatches);
-app.get("/matches-grouped-by-month", matchController.getMatchesByMonth);
-
 console.log("process.env.MONGO_URI ", process.env.MONGO_URI);
-mongoose
-  .connect(process.env.MONGO_URI as string)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Usa la URI correcta para MongoDB
+//TODO arreglar la lectura de la variable de entorno, no está leyendo el .env
+const db = process.env.MONGO_URI || "mongodb://root:secret@localhost:27018/admin";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db as string)
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+const main = async () => {
+  app.listen(PORT, async () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+
+    try {
+      connectDB();
+    } catch (error) {
+      console.error('Unable to connect to the database: ', error);
+    }
+  });
+};
+main();
 
 // Procesar los trabajos en la cola
 myQueue.process(async (job, done) => {
-  try{
+  try {
     console.log(`Processing job ${job.id}:`, job.data);
-    matchService.createMatch(job.data.details.data);
+    // Procesa los datos recibidos de la cola
     done();
-  }catch(error){
-    console.log(`Error in creating Match: ${error}`)
+  } catch (error) {
+    console.log(`Error processing job: ${error}`);
   }
 });
 
