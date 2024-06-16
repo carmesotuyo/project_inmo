2dcc45e0e3d3b9d37d3c32655a2a6ea277905b8c
Fernando Spillere <f.spillere269@gmail.com>
2024-06-16 04:02:46 -0300

    fix de envio de queue

--- InmoServer/src/services/queueService.ts
+++ InmoServer/src/services/queueService.ts
@@ -1,16 +1,13 @@
 import Queue from 'bull';
 import { QueueService } from '../interfaces/services/queueService';
 
-const myQueue = new Queue('properties', 'redis://127.0.0.1:6378');
+const myQueue = new Queue('reports', 'redis://127.0.0.1:6378');
 
 export class QueueServiceImpl implements QueueService {
-  async addJobToQueue(data: JSON): Promise<void> {
+  async addJobToQueue(type: string, data: JSON): Promise<void> {
     try {
-      console.log('Data to be added to queue:', data);
-      const job = await myQueue.add({
-        task: 'sendData',
-        details: { data: data },
-      });
+      console.log(`Data to be added to queue: ${type}`);
+      const job = await myQueue.add({ type: type, data: data });
       console.log(`Job added with ID: ${job.id}`);
     } catch (error) {
       console.error('Failed to add job to the queue:', error);
