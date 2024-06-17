9147320f1e8f28a9cede83f50e11694ce2334868
Fernando Spillere <f.spillere269@gmail.com>
2024-06-16 23:43:21 -0300

    ajustamos la data que se manda en queue

--- InmoServer/src/services/queueService.ts
+++ InmoServer/src/services/queueService.ts
@@ -7,7 +7,10 @@ export class QueueServiceImpl implements QueueService {
   async addJobToQueue(type: string, data: JSON): Promise<void> {
     try {
       console.log(`Data to be added to queue: ${type}`);
-      const job = await myQueue.add({ type: type, data: data });
+      const job = await myQueue.add({ 
+        type: type, 
+        data: data 
+      });
       console.log(`Job added with ID: ${job.id}`);
     } catch (error) {
       console.error('Failed to add job to the queue:', error);
