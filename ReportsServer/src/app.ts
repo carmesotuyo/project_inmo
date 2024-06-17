fca783854bfcd523bf1c0f691d258de5fa423909
Fernando Spillere <f.spillere269@gmail.com>
2024-06-17 04:02:02 -0300

    se agrega servicio para agregar data a la bd

--- ReportsServer/src/app.ts
+++ ReportsServer/src/app.ts
@@ -2,12 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import Queue from 'bull';
+import { createProperty } from './services/propertyService';
+import { createReservation } from './services/reservationService';
+import { createIncident } from './services/incidentService';
+import routes from './routes/routes';
 
 console.log("Started Reports Server");
 
 dotenv.config();
 
 const app = express();
+
+
 const PORT = process.env.PORT || 3001;
 
 // Conectarse a la misma cola que el productor
@@ -16,15 +22,13 @@ const myQueue = new Queue('reports', 'redis://127.0.0.1:6378');
 
 app.use(express.json());
 
-console.log("process.env.MONGO_URI ", process.env.MONGO_URI);
+app.use('/api', routes);
 
-// Usa la URI correcta para MongoDB
-//TODO arreglar la lectura de la variable de entorno, no está leyendo el .env
-const db = process.env.MONGO_URI || "mongodb://root:secret@localhost:27018/admin";
 
+// TODO desacoplar BD
 const connectDB = async () => {
   try {
-    await mongoose.connect(db as string)
+    await mongoose.connect(process.env.MONGO_URI as string)
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
@@ -46,10 +50,26 @@ const main = async () => {
 main();
 
 // Procesar los trabajos en la cola
+// TODO desacoplar queue service
 myQueue.process(async (job, done) => {
   try {
     console.log(`Processing job ${job.id}:`, job.data);
-    // Procesa los datos recibidos de la cola
+    const { type, data } = job.data;
+    
+    switch (type) {
+      case 'property':
+        await createProperty(data);
+        break;
+      case 'reservation':
+        await createReservation(data);
+        break;
+      case 'incident':
+        await createIncident(data);
+        break;
+      default:
+        console.log('Unknown type');
+    }
+    
     done();
   } catch (error) {
     console.log(`Error processing job: ${error}`);
@@ -63,3 +83,4 @@ myQueue.on('completed', (job) => {
 myQueue.on('failed', (job, err) => {
   console.log(`Job ${job.id} failed with error ${err.message}`);
 });
+
