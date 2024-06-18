be2106e8110770bf8b747d6ee4a9324541fc1db2
Fernando Spillere <f.spillere269@gmail.com>
2024-06-18 17:43:10 -0300

    se modifica property service interface

--- InmoServer/src/interfaces/services/propertyService.ts
+++ InmoServer/src/interfaces/services/propertyService.ts
@@ -1,11 +1,12 @@
 import { PropertyRequest } from '../../dtos/propertyRequest';
+import { PropertyResponse } from '../../dtos/propertyResponse';
 import { Property } from '../../data-access/property';
 import { PropertyFilterOptions } from '../../utils/propertyFilters';
 
 export interface PropertyService {
   createProperty(data: PropertyRequest): Promise<InstanceType<typeof Property>>;
   getPropertyByID(id: number): Promise<InstanceType<typeof Property>>;
-  searchProperties(filters: PropertyFilterOptions): Promise<{ properties: InstanceType<typeof Property>[]; total: number }>;
+  searchProperties(filters: PropertyFilterOptions): Promise<{ properties: PropertyResponse[]; total: number }>;
   existsProperty(id: number): Promise<boolean>;
   getAllProperties(): Promise<InstanceType<typeof Property>[]>;
 }
