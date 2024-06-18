567f2b1a818153008e82258bafe4ce17868546f6
carmesotuyo <carmela.sotuyo@gmail.com>
2024-06-18 16:04:05 -0300

    agrego metodo en interfaz

--- InmoServer/src/interfaces/services/incidentService.ts
+++ InmoServer/src/interfaces/services/incidentService.ts
@@ -2,4 +2,5 @@ import { SignalDocument } from '../../data-access/signal';
 
 export interface IncidentService {
   getSignals(): Promise<SignalDocument[]>;
+  getRecentSignalsForProperty(propertyId: string): Promise<SignalDocument[]>;
 }
