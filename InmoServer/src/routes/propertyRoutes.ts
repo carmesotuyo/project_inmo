5ad3b3dfc5d2325f016ccd5b2c61163cb4e4b064
Fernando Spillere <f.spillere269@gmail.com>
2024-06-18 01:24:42 -0300

    se agrega ruta para getAll property

--- InmoServer/src/routes/propertyRoutes.ts
+++ InmoServer/src/routes/propertyRoutes.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.post('/properties', propertyController.createProperty);
 router.get('/properties/search', (req, res) => propertyController.searchProperties(req, res));
+router.get('/properties', (req, res) => propertyController.getAllProperties(req, res));
 
 export default router;
