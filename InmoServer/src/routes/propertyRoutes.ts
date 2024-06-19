df062763c8682fb2949716b7e0b8a866b1b76e17
Fernando Spillere <f.spillere269@gmail.com>
2024-06-19 15:47:03 -0300

    Refactor propertyRoutes to use authMiddleware for searchProperties route

--- InmoServer/src/routes/propertyRoutes.ts
+++ InmoServer/src/routes/propertyRoutes.ts
@@ -4,9 +4,9 @@ import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.post('/properties', authMiddleware(['Propietario']), propertyController.createProperty);
-router.get('/properties/search', (req, res) => propertyController.searchProperties(req, res));
-router.get('/properties', authMiddleware(['Administrador']), (req, res) => propertyController.getAllProperties(req, res));
-router.post('/properties/:id/payment', authMiddleware(['Propietario']), (req, res) => propertyController.payProperty(req, res));
+router.post('/properties', authMiddleware(['Propietario']), propertyController.createProperty); // propietario
+router.get('/properties/search', authMiddleware(), (req, res) => propertyController.searchProperties(req, res)); //todos
+router.get('/properties', authMiddleware(['Administrador']), (req, res) => propertyController.getAllProperties(req, res)); // admin
+router.post('/properties/:id/payment', authMiddleware(['Propietario']), (req, res) => propertyController.payProperty(req, res)); //propietario
 
 export default router;
