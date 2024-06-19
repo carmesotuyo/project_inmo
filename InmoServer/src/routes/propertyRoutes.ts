5252be31bb614539ac3de96185fa46e328983d42
Fernando Spillere <f.spillere269@gmail.com>
2024-06-19 02:24:13 -0300

    Se validan roles

--- InmoServer/src/routes/propertyRoutes.ts
+++ InmoServer/src/routes/propertyRoutes.ts
@@ -4,9 +4,9 @@ import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.post('/properties', propertyController.createProperty);
+router.post('/properties', authMiddleware(['Propietario']), propertyController.createProperty);
 router.get('/properties/search', (req, res) => propertyController.searchProperties(req, res));
-router.get('/properties', (req, res) => propertyController.getAllProperties(req, res));
+router.get('/properties', authMiddleware(['Administrador']), (req, res) => propertyController.getAllProperties(req, res));
 router.post('/properties/:id/payment', authMiddleware(['Propietario']), (req, res) => propertyController.payProperty(req, res));
 
 export default router;
