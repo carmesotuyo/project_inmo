aee5e7abbb00ab07535443460fda8550a0f0dfd5
Fernando Spillere <f.spillere269@gmail.com>
2024-06-19 15:47:13 -0300

    chore: Refactor reservationRoutes to use authMiddleware for all routes

--- InmoServer/src/routes/reservationRoutes.ts
+++ InmoServer/src/routes/reservationRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { reservationController } from '../controllers';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.post('/reservations', reservationController.createReservation);
-router.get('/reservations', reservationController.getReservation); //Get de inquilino
-router.get('/reservations/adm', reservationController.getReservationsAdmin); // Get de admin
-router.post('/reservations/cancel', reservationController.cancelReservation);
+router.post('/reservations',authMiddleware(), reservationController.createReservation); //todos
+router.get('/reservations/:id', authMiddleware(), reservationController.getReservation); //todos
+router.get('/reservations/adm', authMiddleware(['Administrador']), reservationController.getReservationsAdmin); // Get de admin
+router.post('/reservations/cancel', reservationController.cancelReservation); // todos
 export default router;
