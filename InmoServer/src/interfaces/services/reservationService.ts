2049f34ea1af30c0720df95c06d2726a8be5efe8
Fernando Spillere <f.spillere269@gmail.com>
2024-06-19 15:46:48 -0300

    feat: Add email parameter to createReservation method in ReservationService interface

--- InmoServer/src/interfaces/services/reservationService.ts
+++ InmoServer/src/interfaces/services/reservationService.ts
@@ -3,7 +3,7 @@ import { Reservation } from '../../data-access/reservation';
 import { ReservationFilterOptions } from '../../utils/reservationFilters';
 
 export interface ReservationService {
-  createReservation(reservationData: ReservationRequest): Promise<InstanceType<typeof Reservation>>;
+  createReservation(reservationData: ReservationRequest, email: string): Promise<InstanceType<typeof Reservation>>;
   getReservationByEmailAndCode(email: string, reservationCode: string): Promise<InstanceType<typeof Reservation> | null>;
   cancelReservation(email: string, reservationCode: string): Promise<InstanceType<typeof Reservation> | null>;
   getReservationsAdmin(filters: ReservationFilterOptions): Promise<InstanceType<typeof Reservation>[]>;
