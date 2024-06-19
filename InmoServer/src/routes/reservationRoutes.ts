82d004bc2a50aa8d8edafc257003d0cdf60780b3
Fernando Spillere <f.spillere269@gmail.com>
2024-06-19 17:37:59 -0300

    chore: Update reservationRoutes to fix route order and add authMiddleware to getReservation route

--- InmoServer/src/routes/reservationRoutes.ts
+++ InmoServer/src/routes/reservationRoutes.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.post('/reservations',authMiddleware(), reservationController.createReservation); //todos
-router.get('/reservations/:id', authMiddleware(), reservationController.getReservation); //todos
+router.post('/reservations', authMiddleware(), reservationController.createReservation); //todos
 router.get('/reservations/adm', authMiddleware(['Administrador']), reservationController.getReservationsAdmin); // Get de admin
 router.post('/reservations/cancel', reservationController.cancelReservation); // todos
+router.get('/reservations/:id', authMiddleware(), reservationController.getReservation); //todos
 export default router;
