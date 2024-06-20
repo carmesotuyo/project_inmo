1a3fc92943fa6fb2ffc9044af7a576418b307ebe
Fernando Spillere <f.spillere269@gmail.com>
2024-06-19 21:55:36 -0300

    Add payment route and approve reservation route to reservationRoutes

--- InmoServer/src/routes/reservationRoutes.ts
+++ InmoServer/src/routes/reservationRoutes.ts
@@ -7,5 +7,7 @@ const router = Router();
 router.post('/reservations', authMiddleware(), reservationController.createReservation); //todos
 router.get('/reservations/adm', authMiddleware(['Administrador']), reservationController.getReservationsAdmin); // Get de admin
 router.post('/reservations/cancel', reservationController.cancelReservation); // todos
+router.post('/reservations/payment', authMiddleware(), reservationController.paymentCorrect); //todos
+router.post('/reservations/aprove/:id', authMiddleware(['Administrador']), reservationController.aproveReservation); // Get de admin
 router.get('/reservations/:id', authMiddleware(), reservationController.getReservation); //todos
 export default router;
