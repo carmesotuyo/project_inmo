683b82bfcd1b2e0159f59f8be375962801c89e66
Fernando Spillere <f.spillere269@gmail.com>
2024-06-20 04:00:09 -0300

    chore: Add authentication middleware to sensorRoutes

--- InmoServer/src/routes/sensorRoutes.ts
+++ InmoServer/src/routes/sensorRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { sensorController } from '../controllers';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.post('/sensors', (req, res) => sensorController.createSensor(req, res));
-router.post('/sensors/assign', (req, res) => sensorController.assignToProperty(req, res));
-router.get('/sensors/:id/properties', (req, res) => sensorController.getObservableProperties(req, res));
-router.put('/sensors/:id', (req, res) => sensorController.updateSensor(req, res));
+router.post('/sensors',authMiddleware(['Administrador']), (req, res) => sensorController.createSensor(req, res));
+router.post('/sensors/assign',authMiddleware(['Administrador']), (req, res) => sensorController.assignToProperty(req, res));
+router.get('/sensors/:id/properties',authMiddleware(['Administrador']), (req, res) => sensorController.getObservableProperties(req, res));
+router.put('/sensors/:id',authMiddleware(['Administrador']), (req, res) => sensorController.updateSensor(req, res));
 
 export default router;
