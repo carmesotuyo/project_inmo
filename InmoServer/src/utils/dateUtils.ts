aa12e9a581521a4cf577338f20e41579106bb70d
Fernando Spillere <f.spillere269@gmail.com>
2024-06-20 01:58:54 -0300

    chore: Refactor checkDateOverlap function in dateUtils.ts

--- InmoServer/src/utils/dateUtils.ts
+++ InmoServer/src/utils/dateUtils.ts
@@ -5,24 +5,40 @@ export async function checkDateOverlap(
   propertyId: number,
   startDate: string,
   endDate: string,
+  excludeId?: number, // Optional parameter to exclude a specific ID
 ): Promise<boolean> {
-  const overlappingEntries = await model.findAll({
-    where: {
-      propertyId: propertyId,
-      [Op.and]: [
-        {
-          startDate: {
-            [Op.lte]: endDate, // Check if start date is before or on the requested end date
-          },
-        },
-        {
-          endDate: {
-            [Op.gte]: startDate, // Check if end date is after or on the requested start date
-          },
-        },
-      ],
+  const whereClause: any = {
+    status: {
+      [Op.in]: ['Pending Approval', 'Approved', 'Paid'],
     },
+    propertyId: propertyId,
+    [Op.and]: [
+      {
+        startDate: {
+          [Op.lte]: endDate, // Check if start date is before or on the requested end date
+        },
+      },
+      {
+        endDate: {
+          [Op.gte]: startDate, // Check if end date is after or on the requested start date
+        },
+      },
+    ],
+  };
+
+  // Add condition to exclude a specific ID if provided
+  if (excludeId !== undefined) {
+    whereClause.id = {
+      [Op.ne]: excludeId,
+    };
+  }
+
+  const overlappingEntries = await model.findAll({
+    where: whereClause,
   });
+
+  console.log('overlappingEntries: =======');
+  console.log(overlappingEntries);
 
   return overlappingEntries.length > 0;
 }
